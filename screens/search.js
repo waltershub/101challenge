e5a04d4ffbb956e462bdff12645d064c2d60ec9a
waltershub <waltershub@gmail.com>
2018-02-18 22:06:45 -0500

    reactored search and added empty warning

--- screens/search.js
+++ screens/search.js
@@ -19,15 +19,21 @@ class Search extends Component {
     super();
     this.state = {
       query: '',
+      warning: '',
     };
 
     this.searchButtonPress = this.searchButtonPress.bind(this);
   }
 
   searchButtonPress() {
-    if (this.state.query !== '') {
-      this.props.pictureQuery(this.state.query);
+    const query = this.state.query.replace(/\s+/g, '').trim();
+    if (query !== '') {
+      this.props.pictureQuery(query);
       this.props.navigation.navigate('ImageList');
+      this.search.clearText();
+      this.setState({ warning: '' });
+    } else {
+      this.setState({ warning: 'Search cannot be empty' });
       this.search.clearText();
     }
   }
@@ -48,6 +54,7 @@ class Search extends Component {
             style={styles.searchBotton}>
             <Text style={styles.search}>Search</Text>
           </TouchableHighlight>
+          <Text style={styles.warningText}>{this.state.warning}</Text>
         </ImageBackground>
       </View>
     );
@@ -93,10 +100,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   searchBotton: {
-    backgroundColor: 'blue',
+    backgroundColor: '#0000ff',
     height: 50,
     width: 100,
     margin: 20,
     borderRadius: 75,
+  },
+  warningText: {
+    fontSize: 15,
+    color: '#ff0000',
+    fontWeight: 'bold',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2,
   },
 });
